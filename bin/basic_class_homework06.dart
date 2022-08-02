e339d99f713d48a680d8f893795f3407e120081c
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-08-02 11:28:38 +0500

    06

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -6,10 +6,14 @@ Create a "Person" class that has a name("name") and a age("age")
 
 class Person{
 String name='';
-int    age=20;
+int age=20;
+Person({String name='',int age=20}){
+    this.name=name;
+    this.age=age;
+}
 }
 void main(){
-    Person x=Person();
+    Person x=Person(name: 'Ali', age: 20);
     print(x.name);
     print(x.age);
 }
